7844df6f2d9606b32b1f933727845be057496656
Zavodik19 <dzavodov@bk.ru>
2024-05-26 16:38:51 +0300

    ТЗ-6. First version

--- src/test/model/EpicTest.java
+++ src/test/model/EpicTest.java
@@ -4,7 +4,6 @@ import model.Epic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EpicTest {
 
