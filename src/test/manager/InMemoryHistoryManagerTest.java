b6200af0d72a3b279490310dd7e8ff9b9db39509
Zavodik19 <dzavodov@bk.ru>
2024-05-26 16:37:53 +0300

    ТЗ-6. First version

--- src/test/manager/InMemoryHistoryManagerTest.java
+++ src/test/manager/InMemoryHistoryManagerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class InMemoryHistoryManagerTest {
 
